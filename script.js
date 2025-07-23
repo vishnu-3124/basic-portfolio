608e1ed5e295fdab6a07ef46b4364e43184ed80a
Vishnu . C <chandranvishnu2004@gmail.com>
2025-07-23 16:06:21 +0530

    Changed the project info

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+let inceptionProject1 = document.getElementById('inceptionProject1');
+inceptionProject1.style.color = "orange";
+
+let inceptionProject2 = document.getElementById('inceptionProject2');
+inceptionProject2.style.color = "orange";
